e0c460e524303e27413ea18294ad19d0af77fbdd
pawan kumar <t.pawankumar20121999@gmail.com>
2024-06-21 09:08:25 +0530

    Selection Sort

---
+++ SelectionSort1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the size of the array:");
+        int size = Convert.ToInt32(Console.ReadLine());
+        
+        int[] arr = new int[size];
+        
+        Console.WriteLine($"Enter {size} numbers:");
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        SelectionSort selectionSort = new SelectionSort();
+        selectionSort.selection(arr);
+
+        Console.WriteLine("Array elements are:");
+        foreach (int value in arr)
+        {
+            Console.WriteLine(value);
+        }
